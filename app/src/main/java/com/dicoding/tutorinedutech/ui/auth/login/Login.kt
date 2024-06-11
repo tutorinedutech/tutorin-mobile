2260c034a1433fdb62396477b78d1a9d70196d89
wicahma <teguh.dc.kusuma@gmail.com>
2024-06-11 23:15:55 +0700

    update: recreating navigation screen

--- app/src/main/java/com/dicoding/tutorinedutech/ui/auth/login/Login.kt
+++ app/src/main/java/com/dicoding/tutorinedutech/ui/auth/login/Login.kt
@@ -1,18 +1,32 @@
 package com.dicoding.tutorinedutech.ui.auth.login
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.dicoding.tutorinedutech.R
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.dicoding.tutorinedutech.databinding.FragmentLoginBinding
 
 class Login : Fragment() {
 
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            tvAskLogin.setOnClickListener {
+                findNavController().navigate(LoginDirections.actionLogin2ToRegisterMain())
+            }
+        }
     }
 }
--- app/src/main/java/com/dicoding/tutorinedutech/ui/auth/register/main/RegisterMain.kt
+++ app/src/main/java/com/dicoding/tutorinedutech/ui/auth/register/main/RegisterMain.kt
@@ -1,18 +1,43 @@
 package com.dicoding.tutorinedutech.ui.auth.register.main
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.dicoding.tutorinedutech.R
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.dicoding.tutorinedutech.databinding.FragmentRegisterMainBinding
 
 class RegisterMain : Fragment() {
+
+    private var _binding: FragmentRegisterMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_register_main, container, false)
+    ): View {
+        _binding = FragmentRegisterMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            btnSelanjutnyaTutor.setOnClickListener {
+                findNavController().navigate(RegisterMainDirections.actionRegisterMainToRegisterTutor())
+            }
+            btnSelanjutnyaLearner.setOnClickListener {
+                findNavController().navigate(RegisterMainDirections.actionRegisterMainToRegisterLearner())
+            }
+            tvAskLoginTentor.setOnClickListener {
+                findNavController().navigate(RegisterMainDirections.actionRegisterMainToLogin2())
+            }
+            tvAskLoginLearner.setOnClickListener {
+                findNavController().navigate(RegisterMainDirections.actionRegisterMainToLogin2())
+            }
+        }
     }
 
 }

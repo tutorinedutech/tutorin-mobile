fa03a7ad25700373c879b874a79c72bc330e6217
Gabbynts <gabriella.natasya2003@gmail.com>
2024-06-14 18:51:20 +0700

    feat: slicing detail tutor page

--- app/src/main/java/com/dicoding/tutorinedutech/ui/learner/tutors/tutoring/detail/DetailTutoring.kt
+++ app/src/main/java/com/dicoding/tutorinedutech/ui/learner/tutors/tutoring/detail/DetailTutoring.kt
@@ -35,7 +35,7 @@ class DetailTutoring : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_tutoring, container, false)
+        return inflater.inflate(R.layout.fragment_detail_tutoring_learner, container, false)
     }
 
     companion object {

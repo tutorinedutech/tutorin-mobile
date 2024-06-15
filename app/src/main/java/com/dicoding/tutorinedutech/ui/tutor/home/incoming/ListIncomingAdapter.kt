8c79a7e2b92cf124c12bfff4dc706994fd10e28c
wicahma <teguh.dc.kusuma@gmail.com>
2024-06-15 12:21:42 +0700

    update: uninitial thing (too lazy to define it)

---
+++ app/src/main/java/com/dicoding/tutorinedutech/ui/tutor/home/incoming/ListIncomingAdapter.kt
@@ -0,0 +1,20 @@
+package com.dicoding.tutorinedutech.ui.tutor.home.incoming
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+// TODO : Create entity
+//class ListIncomingAdapter() : ListAdapter<ListIncomingAdapter.ListViewHolder>(DIFF_CALLBACK) {
+//
+//
+//    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+//        // TODO: Buat viewholder
+//    }
+//
+//    companion object {
+////        TODO: Buat DiffCallback
+////        val DIFF_CALLBACK
+//    }
+//}
